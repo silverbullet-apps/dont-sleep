d4f60a7509eec6f5ee3f4c5831f6031a96d44bde
Denis Povarov <povarov.denis@gmail.com>
2025-07-11 18:10:05 +0300

    Initial Commit

---
+++ DontSleep/DontSleepApp.swift
@@ -0,0 +1,17 @@
+//
+//  DontSleepApp.swift
+//  DontSleep
+//
+//  Created by Denis Povarov on 11.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct DontSleepApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
